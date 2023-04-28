fa34fcaa2043d3d1386a8970915cff35f76d0e0b
jaime-ribeiro <littlejou@gmail.com>
2023-04-28 18:13:29 -0300

    Corrigindo Validador Enum com o class-validator

--- src/module/alerta/dto/CriarAlerta.dto.ts
+++ src/module/alerta/dto/CriarAlerta.dto.ts
@@ -1,4 +1,4 @@
-import { IsNotEmpty, MaxLength } from 'class-validator';
+import { IsEnum, IsNotEmpty, MaxLength } from 'class-validator';
 import { AlertaTipo } from '../enums/AlertaTipo.enum';
 import { AlertaSituacao } from '../enums/AlertaSituacao.enum';
 import { AlertaStatus } from '../enums/AlertaStatus.enum';
@@ -15,12 +15,18 @@ export class CriarAlertaDTO {
   @IsNotEmpty({ message: 'A descrição não pode ser vazio' })
   descricao: string;
 
+  @IsEnum(AlertaTipo, { message: 'O tipo deve ser "white" ou "black"' })
   @IsNotEmpty({ message: 'O tipo não pode ser vazio' })
   tipo: AlertaTipo;
 
+  @IsEnum(AlertaSituacao, {
+    message:
+      'A situação deve ser "liberação","permissão","bloqueio", "impedimento"',
+  })
   @IsNotEmpty({ message: 'A situação não pode estar vazio' })
   situacao: AlertaSituacao;
 
+  @IsEnum(AlertaStatus, { message: 'O status deve ser "ativo" ou "inativo"' })
   @IsNotEmpty({ message: 'O status não pode ser vazio' })
   status: AlertaStatus;
 }
